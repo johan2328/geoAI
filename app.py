33e63c563761f9c5950d9d2de24ee17d2a3388b2
johan2328 <60123101+johan2328@users.noreply.github.com>
2021-10-25 02:55:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import streamlit.components.v1 as components
 from multipage import MultiPage
 import sys
 warnings.filterwarnings('ignore')
-st.set_page_config(page_title='Centro de entrenamiento GEO-AI',page_icon="🐙",initial_sidebar_state='auto')
+st.set_page_config(page_title='Centro de entrenamiento GEO-AI',initial_sidebar_state='auto')#,page_icon="🐙",
 from pages import carga_data, ml_validacion,visual_data,ml_formulario,ml_masivo# metadata # redundant # importadores de páginas 
 
 hide_st = """
