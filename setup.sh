af3feca43cdd6a90142f8b22c4c653845407153a
johan2328 <60123101+johan2328@users.noreply.github.com>
2021-10-25 02:43:08 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,8 @@
-mkdir -p ~ / .streamlit /
-
-echo "
-[general] \ n
-email =“ your-email@domain.com ”\ n
-"> ~ / .streamlit / credentials.toml
-
-echo "
-[servidor] \ n
-headless = true \ n
-enableCORS = false \ n
-port = $ PORT \ n
-[tema] \ n
-primaryColor =" # 08424f "\ n
-backgroundColor =" # 08424f "\ n
-secundariaBackgroundColor =" # 0d5a6a " \ n
-textColor = " #ffffff " \ n
-font = "sans serif" \ n
-"> ~ / .streamlit / config.toml
+mkdir -p ~/.streamlit/
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
