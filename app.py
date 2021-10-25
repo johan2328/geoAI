40bd6d062e0c9b24d008aef4e34656c350fdb28a
johan2328 <60123101+johan2328@users.noreply.github.com>
2021-10-25 01:17:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ st.markdown(hide_st, unsafe_allow_html=True)
 
 hide_st2= """
             <style>
-                #root {
+                #root .styles_streamlitAppContainer__2rBcU.styles_stateContainer__29Rp6{
                 visibility: hidden;
                 display: none;
                 height: 0%;
