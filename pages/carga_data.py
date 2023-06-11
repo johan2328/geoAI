7b46023d60ff551cc5772c4410080d3397a7f7e9
johan2328 <60123101+johan2328@users.noreply.github.com>
2023-06-11 14:46:09 -0300

    Update carga_data.py

--- pages/carga_data.py
+++ pages/carga_data.py
@@ -13,16 +13,19 @@ def app():
     st.markdown("<p style='text-align: center; color: #DBF2E9;'>Siga los pasos para entrenar en nuestros set de modelos de predictivos</h2>", unsafe_allow_html=True)
     #components.iframe("https://soluciones.aeroterra.com/portal/apps/webappviewer3d/index.html?id=3e5667a5b5634dfaace558c8e672976d",width=700, height=390) 
     st.markdown("#### Cargar data (archivos .csv o .xlsx)")
+    st.dataframe(data)
+    data.to_csv('data/main_data.csv', index=False)
+    for file in uploaded_files:
 
-    uploaded_file = st.file_uploader("Selecciona el archivo", type = ['csv', 'xlsx'],type='csv', accept_multiple_files=False)
-    global data
-    if uploaded_file is not None:
-        try:
-            data = pd.read_csv(uploaded_file,verbose =True,keep_default_na=False,na_values=[''],warn_bad_lines = True, error_bad_lines=False)
-        except Exception as e:
-            print(e)
-            data = pd.read_excel(uploaded_file)
-   
+    bytes_data = file.read()
+
+    st.write("File uploaded:", file.name)
+
+    st.write(bytes_data)
+    
+    
+    
+
 
     ''' Load the data and save the columns with categories as a dataframe. 
     This section also allows changes in the numerical and categorical columns. '''
