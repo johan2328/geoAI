09d437f468cdb2739052c4d85c2ac28ab0cae0e5
johan2328 <60123101+johan2328@users.noreply.github.com>
2023-06-11 14:09:26 -0300

    Update carga_data.py

--- pages/carga_data.py
+++ pages/carga_data.py
@@ -13,8 +13,7 @@ def app():
     st.markdown("<p style='text-align: center; color: #DBF2E9;'>Siga los pasos para entrenar en nuestros set de modelos de predictivos</h2>", unsafe_allow_html=True)
     #components.iframe("https://soluciones.aeroterra.com/portal/apps/webappviewer3d/index.html?id=3e5667a5b5634dfaace558c8e672976d",width=700, height=390) 
     st.markdown("#### Cargar data (archivos .csv o .xlsx)")
-    #with tempfile.NamedTemporaryFile(suffix=".csv") as f:
-    #    f.write("utilizaremos el archivo temporal CSV/XLSX.")
+
     
     # Get the path to the temporary file
     temp_file_path = data.to_csv('data/main_data.csv', index=False)
