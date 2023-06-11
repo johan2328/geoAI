4200ac00060aed04e7e3f13ca49e937ce8af3f6c
johan2328 <60123101+johan2328@users.noreply.github.com>
2023-06-11 15:08:54 -0300

    Update carga_data.py

--- pages/carga_data.py
+++ pages/carga_data.py
@@ -5,7 +5,39 @@ from pages import utils
 from PIL import  Image
 import streamlit.components.v1 as components        
 # import tempfile
+def text_downloader(raw_text):
+	b64 = base64.b64encode(raw_text.encode()).decode()
+	new_filename = "new_text_file_{}_.txt".format(timestr)
+	st.markdown("#### Download File ###")
+	href = f'<a href="data:file/txt;base64,{b64}" download="{new_filename}">Click Here!!</a>'
+	st.markdown(href,unsafe_allow_html=True)
 
+
+def csv_downloader(data):
+	csvfile = data.to_csv()
+	b64 = base64.b64encode(csvfile.encode()).decode()
+	new_filename = "new_text_file_{}_.csv".format(timestr)
+	st.markdown("#### Download File ###")
+	href = f'<a href="data:file/csv;base64,{b64}" download="{new_filename}">Click Here!!</a>'
+	st.markdown(href,unsafe_allow_html=True)
+
+# Class
+class FileDownloader(object):
+	"""docstring for FileDownloader
+	>>> download = FileDownloader(data,filename,file_ext).download()
+	"""
+	def __init__(self, data,filename='myfile',file_ext='txt'):
+		super(FileDownloader, self).__init__()
+		self.data = data
+		self.filename = filename
+		self.file_ext = file_ext
+
+	def download(self):
+		b64 = base64.b64encode(self.data.encode()).decode()
+		new_filename = "{}_{}_.{}".format(self.filename,timestr,self.file_ext)
+		st.markdown("#### Descargar archivo ###")
+		href = f'<a href="data:file/{self.file_ext};base64,{b64}" download="{new_filename}">Click aqui!!</a>'
+		st.markdown(href,unsafe_allow_html=True)
 #@st.cache
 def app():
     st.markdown("", unsafe_allow_html=True)
@@ -14,13 +46,18 @@ def app():
     #components.iframe("https://soluciones.aeroterra.com/portal/apps/webappviewer3d/index.html?id=3e5667a5b5634dfaace558c8e672976d",width=700, height=390) 
     st.markdown("#### Cargar data (archivos .csv o .xlsx)")
      
+    uploaded_file = st.file_uploader("Selecciona el archivo", type = ['csv', 'xlsx'])
+    global data
+    if uploaded_file is not None:
+        try:
+            data = pd.read_csv(uploaded_file,verbose =True,keep_default_na=False,na_values=[''],warn_bad_lines = True, error_bad_lines=False) #,verbose =True,keep_default_na=False,na_values=[''],warn_bad_lines = True, error_bad_lines=False
+        except Exception as e:
+            print(e)
+            data = pd.read_excel(uploaded_file)
     
-    
-
- 
     ''' Load the data and save the columns with categories as a dataframe. 
     This section also allows changes in the numerical and categorical columns. '''
-    '''
+    
     if st.button("Cargar"):
        
         
