b6b9dabb9e6d89489909929e541bf6b7b8fc3a17
johan2328 <60123101+johan2328@users.noreply.github.com>
2023-07-12 17:25:50 -0300

    Update tapex.py

--- pages/tapex.py
+++ pages/tapex.py
@@ -65,6 +65,13 @@ class FileDownloader(object):
 
 #@st.cache
 def app():
+st.title("Falcon-40B Demo")
+prompt = st.text_input("Enter your prompt here:")
+if prompt:
+    max_length = st.slider("Max length", min_value=10, max_value=1000, value=50)
+    output = generate_text(prompt, max_length)
+    st.write(output)
+	
     st.markdown("", unsafe_allow_html=True)
     #st.markdown("<h2 style='text-align: center; color: #2e6c80;'>Predicción de rupturas en red de distribución de aguas</h2>", unsafe_allow_html=True)
     st.markdown("<p style='text-align: center; color: #DBF2E9;'>Siga los pasos para entrenar en nuestros set de modelos de predictivos</h2>", unsafe_allow_html=True)
