6134b9d6b15427cc69d15bd2bb6fa1b94b2a1ca9
johan2328 <60123101+johan2328@users.noreply.github.com>
2023-07-13 17:43:46 -0300

    Update tapex.py

--- pages/tapex.py
+++ pages/tapex.py
@@ -95,7 +95,6 @@ def app():
     st.markdown("Falcon-40B Demo")
     prompt = "Escriba aquí su prompt"
     max_length = 800
-    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
     st.write(generated_text)	
     #prompt = st.text_input("Enter your prompt here:")
     if st.button('Generar texto'):
