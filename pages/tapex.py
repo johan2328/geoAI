dccf32155261a9f73cac1456fc9c6a6ded45db2f
johan2328 <60123101+johan2328@users.noreply.github.com>
2023-07-13 17:24:26 -0300

    Update tapex.py

--- pages/tapex.py
+++ pages/tapex.py
@@ -102,7 +102,7 @@ def app():
     #prompt = st.text_input("Enter your prompt here:")
     if st.button('Generar texto'):
     #	    max_length = st.slider("Max length", min_value=10, max_value=1000, value=50)
-    	    t = threading.Thread(target=run_model)
+    	    t=threading.Thread(target=run_model)
 		#    st.write(t)
             t.start()
 	     #if prompt:
